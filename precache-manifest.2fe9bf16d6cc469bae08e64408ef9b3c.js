3461b722cfc18776bdad6621f41da407b031450c
azenkurt <azenkurit@gmail.com>
2019-11-18 17:50:27 +0300

    Updates

---
+++ precache-manifest.2fe9bf16d6cc469bae08e64408ef9b3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931f47aed009dee1d5be37a041d3b83f",
+    "url": "/azenkurt/cabinet/index.html"
+  },
+  {
+    "revision": "3777569e91680b786436",
+    "url": "/azenkurt/cabinet/static/css/main.e2ed35e6.chunk.css"
+  },
+  {
+    "revision": "b340b5b7e69b5a30be37",
+    "url": "/azenkurt/cabinet/static/js/2.e3df1397.chunk.js"
+  },
+  {
+    "revision": "3777569e91680b786436",
+    "url": "/azenkurt/cabinet/static/js/main.60cd2c2e.chunk.js"
+  },
+  {
+    "revision": "dd1ad338ba75329b56bb",
+    "url": "/azenkurt/cabinet/static/js/runtime-main.c07fd734.js"
+  }
+]);
