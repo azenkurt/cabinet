9c1a6ca23cc081f7cb3b59da236a2f13fce1f8f6
azenkurt <azenkurit@gmail.com>
2019-11-19 17:52:17 +0300

    showScreen

--- src/components/NPB/index.js
+++ src/components/NPB/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './NPB.css';
 
+import ShowScreenPdf from '../ShowScreenPdf';
+
 class NPB extends React.Component {
     render() {
         return (
             <div className = "NPB">
                <p className = "NPB_text">doc0</p>
-               <a href = "./pdf/nrb/1.pdf">asdasd</a>
+               <ShowScreenPdf 
+                    showSrc = "/pdf/Npb/1.pdf"
+                    showWidth = "900px"
+                    showHeight = "700px"
+                    type = "application/pdf" />
             </div>
         )
     }
